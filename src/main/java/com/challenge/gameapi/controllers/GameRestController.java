eaf9edc5520e4316edfa88d9bc638e78349f1775
barjinderpaul <barjinderpaul.singh@gmail.com>
2020-01-11 13:17:31 +0530

    Added logging

--- src/main/java/com/challenge/gameapi/controllers/GameRestController.java
+++ src/main/java/com/challenge/gameapi/controllers/GameRestController.java
@@ -31,13 +31,17 @@ public class GameRestController {
 
     @GetMapping(value = "/")
     public ModelAndView renderSwaggerPage(){
+        log.info(getClass() + " render Home Page");
         return new ModelAndView("redirect:/swagger-ui.html#/");
     }
 
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ResponseEntity<SuccessMessage> uploadMultipart(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info(getClass() + " file upload started");
+
         gameService.saveAll(CsvUtils.read(Game.class, file.getInputStream()));
 
+        log.info(getClass() + " file upload done");
         HttpStatus httpStatus = HttpStatus.CREATED;
         SuccessMessage successMessage = new SuccessMessage(200,"File successfully uploaded",httpStatus);
         return new ResponseEntity<>(successMessage,httpStatus);
@@ -47,18 +51,23 @@ public class GameRestController {
 
     @GetMapping(value="/games")
     public List<Game> getAllGames(@RequestParam(value = "title",required = false )String title){
+        log.info(getClass() + " fetching all games");
         if(title == null ){
             List<Game> games = gameService.findAll();
             return games;
         }
         else {
+            log.info(getClass() + " fetched all games successfully");
             return gameService.findAllByTitle(title);
         }
     }
 
     @GetMapping(value = "/games/{id}")
     public Game getSingleGame(@PathVariable("id")String id){
-        return gameService.getSingleGame(id);
+        log.info(getClass() + " fetching game with id: "+ id);
+        Game game = gameService.getSingleGame(id);
+        log.info(getClass() + " fetched game with id: " + id);
+        return game;
     }
 
     @PostMapping(value = "/games")
@@ -68,8 +77,9 @@ public class GameRestController {
                                      @RequestParam(value = "genre", required = false) String genre,
                                      @RequestParam(value = "editorsChoice", required = false) String editorsChoice) {
 
+        log.info(getClass() + " creating new post ");
         gameService.saveGame(title, platform, score, genre, editorsChoice);
-
+        log.info(getClass() + " new post created");
         HttpStatus httpStatus = HttpStatus.CREATED;
         SuccessMessage successMessage = new SuccessMessage(200,"Post created successfully",httpStatus);
         return new ResponseEntity<>(successMessage,httpStatus);
@@ -82,8 +92,11 @@ public class GameRestController {
                            @RequestParam(value = "platform", required = false) String platform,
                            @RequestParam(value = "genre", required = false) String genre,
                            @RequestParam(value = "editorsChoice", required = false) String editorsChoice){
+        log.info(getClass() + " PUT /games/"+id);
+
         gameService.updateGame(id, title, platform, score, genre, editorsChoice);
 
+        log.info(getClass() + " successfully updated game with id: " + id);
         HttpStatus httpStatus = HttpStatus.OK;
         SuccessMessage successMessage = new SuccessMessage(200,"Post updated successfully",httpStatus);
         return new ResponseEntity<>(successMessage,httpStatus);
@@ -96,8 +109,11 @@ public class GameRestController {
                            @RequestParam(value = "platform", required = false) String platform,
                            @RequestParam(value = "genre", required = false) String genre,
                            @RequestParam(value = "editorsChoice", required = false) String editorsChoice){
+        log.info(getClass() +  " PATCh /games/"+id);
+
         gameService.updateGamePatch(id, title, platform, score, genre, editorsChoice);
 
+        log.info(getClass() + " successfully updated game with id : " + id);
         HttpStatus httpStatus = HttpStatus.OK;
         SuccessMessage successMessage = new SuccessMessage(200,"Post updated successfully",httpStatus);
         return new ResponseEntity<>(successMessage,httpStatus);
@@ -105,7 +121,11 @@ public class GameRestController {
 
     @DeleteMapping(value = "/games/{id}")
     public ResponseEntity<Object> deleteGame(@PathVariable("id") String id){
+        log.info(getClass() + " deleting game with id :" + id);
+
         gameService.deleteGame(id);
+
+        log.info(getClass() + " successfully deleted post with id : " + id);
 
         HttpStatus httpStatus = HttpStatus.OK;
         SuccessMessage successMessage = new SuccessMessage(200,"Post deleted successfully",httpStatus);
